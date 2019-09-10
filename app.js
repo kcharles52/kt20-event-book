1826997f7e66f44f8cc5d0c6e6d542c1f5218a5b
Kato Charles <kcharles52@gmail.com>
2019-09-10 18:14:40 +0300

    chore(api-setup): graphql api setup (#2)
    - add graphql packages
    - add schema and resolvers

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
 
-app.get("/", (req, res, next) => {
-  res.send("This is the initial page");
-});
+      type RootMutation {
+        createEvent(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation:RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ['All-Night coding', 'Dating', 'code-review'];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(5001);
