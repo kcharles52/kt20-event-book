3b8a3c9c13943869b494c1cdb93289d6bcad42fe
Charles Kato <kcharles52@gmail.com>
2019-09-12 16:51:28 +0300

    feature(user): register user
    - add user model
    - add user type
    - add password encryptiom
    - add relationship between user and event modal

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 // modals
 const Event = require('./models/event');
+const User = require('./models/user');
 
 // create an express application
 const app = express();
@@ -23,6 +25,16 @@ app.use(
         date: String!
       }
 
+      type User {
+        email: String!
+        password: String
+      }
+
+      input UserInput {
+        email: String!
+        password: String!
+      }
+
       input EventInput {
         title: String!
         description: String!
@@ -36,6 +48,7 @@ app.use(
 
       type RootMutation {
         createEvent(eventInput: EventInput): Event
+        createUser(userInput: UserInput): User
       }
 
       schema {
@@ -45,25 +58,67 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return Event.find({}).then(events => {
-          return events
-        }).catch(err => { throw err })
+        return Event.find({})
+          .then((events) => {
+            return events;
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
       createEvent: (args) => {
         const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: new Date(args.eventInput.date)
+          date: new Date(args.eventInput.date),
+          creator: '5d7a46dc707ee632199f8a0e'
         });
+        let createdEvent;
 
         return event
           .save()
           .then((result) => {
-            return { ...result._doc };
+            createdEvent = result._doc;
+            return User.findById('5d7a46dc707ee632199f8a0e');
+          })
+          .then((user) => {
+            if (!user) {
+              throw new Error('User not found');
+            }
+
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(() => {
+            return createdEvent;
           })
           .catch((err) => {
             console.log(err);
+            throw err;
+          });
+      },
+      createUser: (args) => {
+        return User.findOne({ email: args.userInput.email })
+          .then((user) => {
+            if (user) {
+              throw new Error('User exists already, please login');
+            }
+
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then((hashedPassword) => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+
+            return user.save();
+          })
+          .then((result) => {
+            return { ...result._doc, password: null };
+          })
+          .catch((err) => {
             throw err;
           });
       }
@@ -77,7 +132,7 @@ const url =
   process.env.MONGODB_URL ||
   process.env.DB_URL ||
   `mongodb://localhost:27017/${process.env.MONGO_DB}`;
-const port = process.env.PORT
+const port = process.env.PORT;
 
 mongoose
   .connect(url, { useNewUrlParser: true })
